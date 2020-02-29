6523f1f719e5618d1fa303da7f53291c0cfbac85
Abhijit <contact.me.abhijit@gmail.com>
2020-02-29 19:03:52 +0530

    Just sample commit, nothing exciting...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import CakeContainer from "./components/cakeContainer";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <p>Hello World!</p>
+      <CakeContainer />
     </div>
   );
 }
---
+++ src/components/cakeContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CakeContainer = () => {
+  return <p>Cake Store</p>;
+};
+
+export default CakeContainer;
